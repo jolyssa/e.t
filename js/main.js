09e4621c95c5e6aab5a6b025bdc0eebc353b8a19
jolyssa <jolyssa.armstrong@gmail.com>
2024-02-19 01:08:04 -0500

    added typewriter effect

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
-// const i = 0
-let text = document.querySelector('#typewriter').textContent
+const typewriterText = document.querySelector('#typewriter')
 
-console.log(text)
+const phrases = [
+    'Welcome to Elephant Thoughts 🐘',
+    'Bienvenue a Elephant Thoughts 🐘',
+    'Need tutoring? We got you.'
+]
 
+let phraseIndex = 0
+let characterIndex = 0
 
-function typeWriterEffect(){
-    if (i < text.length) {
-        document.querySelector('#typewriter').innerHTML += text.charAt(i);
-        i++;
-        setTimeout(typeWriterEffect, speed);
+function type(){
+    if (characterIndex < phrases[phraseIndex].length) {
+        typewriterText.textContent += phrases[phraseIndex][characterIndex]
+        characterIndex++
+        setTimeout(type, 100)
+    } else {
+        setTimeout(erase, 1000)
     }
 }
-   
+
+function erase(){
+    if (characterIndex > 0) {
+        typewriterText.textContent = phrases[phraseIndex].substring(0, characterIndex - 1)
+        characterIndex--
+        setTimeout(erase, 50)
+      } else {
+        phraseIndex = (phraseIndex + 1) % phrases.length;
+        setTimeout(type, 250)
+      }
+    }
+
+    type()
